476d286e2268d8bd7e8e66e5b2762c5d40b5c266
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2026-07-16 13:26:51 +0300

    1326

--- src/bloodhound_gang/classes/watchdogs/watchdog_source.py
+++ src/bloodhound_gang/classes/watchdogs/watchdog_source.py
@@ -143,7 +143,7 @@ class WatchdogSource(WatchdogBasic):
             # Сравниваем и обрабатываем изменения, new_tree мутирует
             self.logger.info('Произведено повторное сканирование для %s', self.source_folder.as_posix())
             changed = await self._compare_and_process_tree(
-                                                    old=old_tree['tree'],
+                                                    old=old_tree,
                                                     new=new_tree['tree'],
                                                     base_path=self.source_folder,
                                                     depth=0
