0f5101e2523be0d807c6ed41f38ad084beae9adb
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2026-07-18 00:29:03 +0300

    0029

--- src/bloodhound_gang/classes/objects/process.py
+++ src/bloodhound_gang/classes/objects/process.py
@@ -286,7 +286,7 @@ class Process(BaseModel):
         # process_id parsing
         task_id, task_name, task_version, sample_id, tags = decode_process_id(process_id)
 
-        #logger.debug("Sample:\n%s", sample.to)
+        logger.debug("Sample:\n%s", sample.to_db())
         
         process_data.update({
                              'res_d': sample.res_d.joinpath(task_name, *tags, task_version),
